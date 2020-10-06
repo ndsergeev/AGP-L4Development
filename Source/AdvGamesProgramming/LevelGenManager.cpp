c4063feb95e374e22441003b9d02e96038958817
nsergeev <sergeevkep@icloud.com>
2020-10-06 17:08:44 +1100

    Added: Room Traverse, see full update
    Please change parameters of ARoom directly from the actor in the scene now
    
    ToDo:
    Please, use references, auto&, auto* to simplify the reading
    Please, use Unreal Docs to find the path of the library you plan to include, those are consistent between systems.
    When creating new AActor, untick if it is not something movable or controllable.
    It is better to avoid AActor at all and use a custom class for calculating all of these things and THEN spawn rooms in the positions
    Also, we need to restretch tiles, the new mesh is coming in the next update.

--- Source/AdvGamesProgramming/LevelGenManager.cpp
+++ Source/AdvGamesProgramming/LevelGenManager.cpp
@@ -1,24 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Room.h"
 #include "Engine/World.h"
 #include "LevelGenManager.h"
 
-// Sets default values
 ALevelGenManager::ALevelGenManager()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    /**
+     * Matt, please, do not forget to unTick actors
+     */
+	PrimaryActorTick.bCanEverTick = false;
 }
 
-// Called when the game starts or when spawned
 void ALevelGenManager::BeginPlay()
 {
 	Super::BeginPlay();
 	GenerateLevel();
 }
 
-// Called every frame
 void ALevelGenManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -26,9 +23,36 @@ void ALevelGenManager::Tick(float DeltaTime)
 
 void ALevelGenManager::GenerateLevel()
 {
-	ARoom* InitialRoom = GetWorld()->SpawnActor<ARoom>();
-	InitialRoom->SetSize(0, 60, 60, 0);
+	auto* InitialRoom = GetWorld()->SpawnActor<ARoom>();
+	InitialRoom->SetSize(Left, Right, Top, Bottom);
 	InitialRoom->Split();
 	InitialRoom->AddCorridors();
 	InitialRoom->DrawRoom();
+
+    TraverseRooms(InitialRoom);
+
+#ifdef UE_EDITOR
+    UE_LOG(LogTemp, Warning, TEXT("Rooms Number: %i"), Rooms.Num());
+    for (const auto& Room : Rooms)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("<%s>\tW:%i,\t\tH:%i"),
+               *Room->GetName(),
+               Room->GetWidth(),
+               Room->GetHeight());
+    }
+#endif
+}
+
+void ALevelGenManager::TraverseRooms(ARoom* Room)
+{
+    if (!Room) return;
+
+    TraverseRooms(Room->LeftRoom);
+
+    TraverseRooms(Room->RightRoom);
+
+    if (Room->IsLeaf())
+    {
+        Rooms.Add(Room);
+    }
 }
--- Source/AdvGamesProgramming/LevelGenManager.h
+++ Source/AdvGamesProgramming/LevelGenManager.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -13,18 +11,27 @@ class ADVGAMESPROGRAMMING_API ALevelGenManager : public AActor
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	ALevelGenManager();
 
+    UPROPERTY(EditAnywhere)
+        int Left;
+    UPROPERTY(EditAnywhere)
+        int Right;
+    UPROPERTY(EditAnywhere)
+        int Top;
+    UPROPERTY(EditAnywhere)
+        int Bottom;
+
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	TArray<ARoom*> Rooms;
 
 	void GenerateLevel();
+
+private:
+    void TraverseRooms(ARoom* Room);
 };
